de1eeeb966f3e73489c682da1780333919c3ec75
Shivam <shivam123ortan@gmail.com>
2022-08-21 13:22:23 +0530

    Added String

---
+++ 8_Strings/StringBuilder/StringBuilder.java
@@ -0,0 +1,28 @@
+
+public class StringBuilder {
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder();
+        for(char ch='a'; ch<='z'; ch++){
+            sb.append(ch);
+        }
+        System.out.println(sb);
+        
+        // Not allowed -->
+        // int a = 10;
+        // a.toString();
+
+        // Allowed -->
+        // Integer a = 10;
+        // a.toString();
+
+        // Not Allowed -->
+        // char ch = 'a';
+        // ch.toString();
+
+        // Allowed -->
+        // Character ch = 'a';
+        // ch.toString();
+        
+    }    
+}
